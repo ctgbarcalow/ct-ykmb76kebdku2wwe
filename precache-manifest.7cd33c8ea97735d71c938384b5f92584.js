bf562ad8e0abdb9889f582eabe27fc31c6fd8621
Geoff Barcalow <geoffb@intra.ctww.org>
2021-06-10 10:52:42 -0700

    Updates

---
+++ precache-manifest.7cd33c8ea97735d71c938384b5f92584.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5afafb6b6a0f52cab7436ea041f34171",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f672806f7f3e14ed969f",
+    "url": "./static/css/main.b693b373.chunk.css"
+  },
+  {
+    "revision": "36203b02a6e7b49934db",
+    "url": "./static/js/2.24ef3e35.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.24ef3e35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f672806f7f3e14ed969f",
+    "url": "./static/js/main.3f2eed2b.chunk.js"
+  },
+  {
+    "revision": "25afd14ebfd72c06683e",
+    "url": "./static/js/runtime-main.cc9ba5f5.js"
+  }
+]);
